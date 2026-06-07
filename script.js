d8c933052f5d205c4322c97934383df1bb3d59ae
alsachokal-eng <alsachokal@gmail.com>
2026-06-07 16:14:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Order form script loaded");
   var form = document.getElementById("order-form");
   var submitButton = document.getElementById("order-submit");
-  var webhookUrl = "https://hook.eu1.make.com/YOUR_WEBHOOK_ID"; // Замініть на свій Make webhook URL
+  var webhookUrl = "https://hook.eu1.make.com/bpj0xrlabzcvknkbd67v4up2wc212n7e"; // Замініть на свій Make webhook URL
 
   if (!form) {
     console.error("Order form not found");
